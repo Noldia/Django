3d1ef8ea693861b3488e410618fb683a515ff65c
Noldia Chavez <noldia12@gmail.com>
2022-03-25 16:25:43 -0500

    Forms_introduction

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,9 @@
+from tracemalloc import get_object_traceback
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latst_question_list = Question.objects.all()
@@ -18,8 +20,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Result's question number {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Voting question number {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
